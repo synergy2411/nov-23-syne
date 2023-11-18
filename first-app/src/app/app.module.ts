b1a2fce5cd98387dfac4031de77e171f85591023
Sumit Khandelwal <synergy2411@outlook.com>
2023-11-18 15:12:49 +0530

    parent to child

--- first-app/src/app/app.module.ts
+++ first-app/src/app/app.module.ts
@@ -4,12 +4,16 @@ import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { UsersComponent } from './components/users/users.component';
+import { ParentComponent } from './components/demo/parent/parent.component';
+import { ChildComponent } from './components/demo/child/child.component';
 
 @NgModule({
   declarations: [
     // Components | Directives | Pipes
     AppComponent,
     UsersComponent,
+    ParentComponent,
+    ChildComponent,
   ],
   imports: [
     // Modules - Built-in | Custom Modules
