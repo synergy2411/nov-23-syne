a36ace02116722c8a731028a4b775c9b235f3aef
Sumit Khandelwal <synergy2411@outlook.com>
2023-11-25 11:41:07 +0530

    reactive form added

--- first-app/src/app/components/auth/auth.component.ts
+++ first-app/src/app/components/auth/auth.component.ts
@@ -1,15 +1,27 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-auth',
   templateUrl: './auth.component.html',
-  styleUrls: ['./auth.component.css']
+  styleUrls: ['./auth.component.css'],
 })
 export class AuthComponent implements OnInit {
+  username = new FormControl();
+  password = new FormControl();
 
-  constructor() { }
+  authForm: FormGroup;
 
-  ngOnInit(): void {
+  constructor(private fb: FormBuilder) {
+    this.authForm = this.fb.group({
+      username: this.username,
+      password: this.password,
+    });
   }
 
+  ngOnInit(): void {}
+
+  onLogin() {
+    console.log(this.authForm);
+  }
 }
